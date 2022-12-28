629cfde03934f48ce8fe9a3b9f1db79601c8e492
Zokirkhon <zokirxonkotibxanov@gmail.com>
2022-12-29 03:05:42 +0900

    oldingi holatiga qaytarib qo'ydim

--- src/index.js
+++ src/index.js
@@ -2,26 +2,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style/index.css";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-
 import "./static/methods";
 import "number-brm";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { persister, store } from "./persist";
+import {persister,store} from "./persist"
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persister}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persister}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   // </React.StrictMode>
 );
 
